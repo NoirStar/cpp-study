418225d6ae10496c8473c7380d0da264f246fbfd
namwoo.kim <sky_9233@naver.com>
2021-06-20 15:10:34 +0900

    study

---
+++ 20210620/main.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <cmath>
+#include <vector>
+
+
+using namespace std;
+
+// void getSinCos(const double &degree, double &sin_out, double &cos_out) {
+//     static const double pi = 3.141592;
+//     double radians = degree * pi / 180.0;
+//     sin_out = sin(radians);
+//     cos_out = cos(radians);
+// }
+// int main() {
+    
+
+//     double sin (0.0);
+//     double cos (0.0);
+
+//     getSinCos(30.0, sin, cos);
+
+//     cout << sin << cos << endl;
+
+//     return 0;
+
+// }
+// typedef int* pint;
+
+// void foo(pint &x) {
+//     cout << x<< endl;
+// }
+
+// int main() {
+//     int x = 5;
+//     pint ptr = &x;
+    
+    
+
+//     return 0;
+// }
+
+void printElements(const vector<int> &arr) {
+
+}
+
+
+int main() {
+    vector<int> arr{1,2,3,4};
+    return 0;    
+}
