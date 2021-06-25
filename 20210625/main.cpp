bf06c4a8276330edec012834c7ee325d8d3d8f95
namwoo.kim <sky_9233@naver.com>
2021-06-25 23:14:15 +0900

    study

---
+++ 20210625/main.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <vector>
+#include <cassert>
+
+using namespace std;
+
+
+// void printStack(const vector<int> &stack) {
+//     for (auto &e : stack) {
+//         cout << e << " ";
+//     }
+// }
+
+// int main() {
+
+//     vector<int> stack;
+    
+//     stack.push_back(3);
+//     printStack(stack);
+//     stack.push_back(5);
+//     printStack(stack);
+//     stack.push_back(7);
+//     printStack(stack);
+//     stack.pop_back();
+//     printStack(stack);
+//     stack.pop_back();
+//     printStack(stack);
+//     stack.pop_back();
+//     printStack(stack);
+
+//     return 0;
+// }
+
+// void countDown(int count) {
+//     cout << count << endl;
+//     countDown(count -1);
+    
+//     if(count > 0) {
+//         countDown(count -1);
+//     }
+// }
+
+// int main () {
+
+//     countDown(5);
+//     return 0;
+// }
+
+int main () {
+
+    // const int x = 10;
+    // assert (x==5);
+    // static_assert(x==5, "x should be 5");
+    return 0;
+}
